e07d02b33807adc3f616078b50601feae07893ae
Daniel Ruiz <danielruimar@gmail.com>
2023-10-17 22:02:28 +0200

    f

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Formulario from './pages/Formulario';
 import Correct from './pages/Correct';
 
+import Calabaza from './assets/img/calabaza-smile.svg'; // Importa la imagen SVG
+
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simula un tiempo de carga (puedes reemplazar esto con tu lógica de carga real)
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); // Cambia el tiempo según tus necesidades
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className='loading'>
+        <img className='spinner' src={Calabaza} alt='Cargando...' />
+      </div>
+    );
+  }
+
   return (
     <HashRouter>
       <div className='App'>
