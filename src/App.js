9437e160415b2b0a7293901ec1765e58773ba23c
Daniel Ruiz <danielruimar@gmail.com>
2023-10-18 09:36:20 +0200

    F

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, lazy, Suspense } from 'react';
+import React, { lazy, Suspense } from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Loading from './components/Loading';
 
@@ -7,29 +7,12 @@ const Formulario = lazy(() => import('./pages/Formulario'));
 const Correct = lazy(() => import('./pages/Correct'));
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [isHomeLoaded, setIsHomeLoaded] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 3000);
-
-    setTimeout(() => {
-      setIsHomeLoaded(true);
-    }, 3000);
-  }, []);
-
-  if (isLoading) {
-    return <Loading />;
-  }
-
   return (
     <HashRouter>
       <div className='App'>
         <Suspense fallback={<Loading />}>
           <Routes>
-            {isHomeLoaded && <Route path='/' element={<Home />} />}
+            <Route path='/' element={<Home />} />
             <Route path='/formulario' element={<Formulario />} />
             <Route path='/correct' element={<Correct />} />
             <Route path='/*' element={<div>Página no existe</div>} />
